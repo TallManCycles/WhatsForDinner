6674b84a44fd2ebeb57dd11814d739c5c876cf50
Aaron Day <aaronday@live.com.au>
2015-08-19 20:18:02 +1000

    Initil commit

---
+++ src/WhatsForDinnerBlank/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using WhatsForDinnerBlank.Model;
+
+namespace WhatsForDinnerBlank.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new List<Recipes>();
+            model.Add(new Recipes { Description = "Dinner 1", Rating = 5, DinnerTime = DateTime.Parse("2015-08-17 06:30:00.000") });
+            model.Add(new Recipes { Description = "Dinner 2", Rating = 6, DinnerTime = DateTime.Parse("2015-08-17 06:30:00.000") });
+
+            var result = model.OrderBy(i => i.Rating).ToList();
+
+            return View(result);
+        }
+
+        public IActionResult Vote(object description)
+        { 
+            var model = new List<Recipes>();
+            model.Add(new Recipes { Description = "Dinner 1", Rating = 5, DinnerTime = DateTime.Parse("2015-08-17 06:30:00.000") });
+            model.Add(new Recipes { Description = "Dinner 2", Rating = 6, DinnerTime = DateTime.Parse("2015-08-17 06:30:00.000") });
+
+            model.Where(item => item.Description == description.ToString()).First().Rating = + 1;
+
+            return View(model);
+        }
+    }
+}
---
+++ src/WhatsForDinnerBlank/Model/Recipes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WhatsForDinnerBlank.Model
+{
+    public class Recipes
+    {
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public DateTime DinnerTime { get; set; }
+    }
+}
